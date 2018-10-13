cf61eeff0d84c752f22732b5f488d5eb55069578
dohsimpson <mydohsimpson@gmail.com>
2018-10-13 18:53:24 -0400

    Work pls!

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cd ~
 ! hash git 2>/dev/null && sudo apt-get install -y git
 git init
 git remote add origin "https://github.com/dohsimpson/dotfiles"
-git pull "https://github.com/dohsimpson/dotfiles" master
+git fetch --all
+git reset --hard origin/master
 
 vim +PlugInstall +qall > /dev/null
